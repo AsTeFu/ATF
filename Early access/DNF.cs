de4d2cab70d1b1769f3e83d767ca4b973dae4019
Хвостенко Вячеслав <40929439+AsTeFu@users.noreply.github.com>
2018-12-14 01:22:59 +0600

    Add files via upload

---
+++ Early access/DNF.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DNF {
+    static class Program {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+            
+            
+        }
+    }
+
+    class Function {
+        private bool[] function;
+        private int[] functionInt;
+
+        private int countVariables;
+        private int rows {
+            get {
+               return (int)Math.Pow(2, countVariables);
+            }
+        }
+
+        private bool[,] truthTable;
+
+        private char overline = '̅';
+
+        public bool ClassT0 {
+            get {
+                return !function[0];
+            }
+        }
+        public bool ClassT1 {
+            get {
+                return function[rows - 1];
+            }
+        }
+        public bool ClassS {
+            get {
+                return this == GetDualFunction();
+            }
+        }
+        public bool ClassM {
+            get {
+                bool isComparable(int first, int second) {
+                    for (int i = 0; i < countVariables; i++) {
+                        if ((truthTable[first, i] ? 1 : 0) > (truthTable[second, i] ? 1 : 0))
+                            return false;
+                    }
+                    return true;
+                }
+
+                for (int i = 0; i < rows; i++) {
+                    if (function[i]) {
+                        for (int j = i + 1; j < rows; j++) {
+                            if (isComparable(i, j) && !function[j])
+                                return false;
+                        }
+                    }
+                }
+                
+                return true;
+            }
+        }
+        public bool ClassL {
+            get {
+                bool isConjunction(int index) {
+                    int tmp = 0;
+                    for (int i = 0; i < countVariables; i++) {
+                        if (truthTable[index, i])
+                            tmp++;
+                    }
+
+                    return tmp > 1;
+                }
+
+                bool[] linear = new bool[rows];
+                bool[] lineartwo = new bool[rows];
+
+                List<bool> polynomial = new List<bool>();
+
+                polynomial.Add(function[0]);
+                for (int i = 0; i < rows; i++) {
+                    linear[i] = function[i];
+                }
+                
+                for (int i = 1; i < rows; i++) {
+                    if (i % 2 == 1) {
+                        lineartwo = new bool[rows - i];
+                        polynomial.Add(linear[0] != linear[1]);
+
+                        for (int j = 0; j < rows - i; j++) {
+                            lineartwo[j] = linear[j] != linear[j+1];
+                        }
+                    } else {
+                        linear = new bool[rows - i];
+                        polynomial.Add(lineartwo[0] != lineartwo[1]);
+
+                        for (int j = 0; j < rows - i; j++) {
+                            linear[j] = lineartwo[j] != lineartwo[j + 1];
+                        }
+                    }
+                    
+                }
+                
+                for (int i = 0; i < polynomial.Count; i++) {
+                    if (polynomial[i] && isConjunction(i))
+                        return false;
+                }
+
+                return true;
+            }
+        }
+        
+        public static Function conjuncture = new Function(new int[] { 0, 0, 0, 1 });
+        public static Function disjunction = new Function(new int[] { 0, 1, 1, 1 });
+        public static Function implication = new Function(new int[] { 1, 1, 0, 1 });
+
+
+        private string PDNF {
+            get {
+                return GetPDNF();
+            }
+        }
+        List<char> namesVariables = new List<char>();
+                
+        private Function (int countVariables) {
+            this.countVariables = countVariables;
+
+            function = new bool[(int)Math.Pow(2, countVariables)];
+            functionInt = new int[(int)Math.Pow(2, countVariables)];
+
+            truthTable = GetTableTruthBinary(countVariables);
+        }
+        private Function(int[] func) : this((int)Math.Log(func.Length, 2)) {
+            functionInt = func;
+
+            bool[] f = new bool[func.Length];
+            for (int i = 0; i < f.Length; i++) {
+                f[i] = func[i] == 1;
+            }
+            function = f;
+            
+            for (int i = 0; i < countVariables; i++) {
+                namesVariables.Add((char)('x' + i));
+            }
+
+        }
+        public Function(int[] func, char[] names) : this(func) {
+            namesVariables = new List<char>(names);
+        }
+
+        private static string GetBinaryNumber(int num, int countVariables) {
+            string ConvertNumber(string number, int count) {
+                string tmp = "";
+                for (int i = number.Length; i < count; i++) {
+                    tmp += "0";
+                }
+                return tmp + number;
+            }
+
+            string result = "";
+            do {
+                result = (num % 2) + result;
+                num = num / 2;
+            } while (num > 0);
+
+            return ConvertNumber(result, (int)Math.Pow(2, countVariables));
+        }
+        private bool[,] GetTableTruth(int countVariables) {
+
+            //int rows = (int)Math.Pow(2, countVariables);
+            bool[,] table = new bool[rows, countVariables];
+
+            bool tmp;
+            int keyIndex;
+            for (int j = 1; j <= countVariables; j++) {
+                tmp = false;
+                keyIndex = (int)Math.Pow(2, j - 1) + 1;
+
+                for (int i = 0; i < rows; i++) {
+
+                    if ((i + 1) == keyIndex) {
+                        tmp = !tmp;
+                        keyIndex += (int)Math.Pow(2, j - 1);
+                    }
+
+                    truthTable[i, countVariables - j] = tmp;
+
+                }
+            }
+
+            return table;
+
+        }
+        private bool[,] GetTableTruthBinary(int countVariables) {
+
+            //int rows = (int)Math.Pow(2, countVariables);
+            bool[,] table = new bool[rows, countVariables];
+            
+            for (int i = 0; i < rows; i++) {
+                string binaryNumber = GetBinaryNumber(i, countVariables);
+
+                for (int j = 0; j < countVariables; j++) {
+                    table[i, j] = int.Parse(binaryNumber[j] + "") == 1;
+                }
+            }
+
+            return table;
+        }
+
+        public string TruthTable() {
+            int IsTrue(bool varialbe) {
+                return varialbe ? 1 : 0;
+            }
+
+            string table = "";
+
+            for (int j = 0; j < countVariables; j++) {
+                table += "|\t" + namesVariables[j] + "\t";
+            }
+            table += "|\t" + "F" + "\t|" + Environment.NewLine + Environment.NewLine;
+            
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < countVariables; j++) {
+                    table += "|\t" + IsTrue(truthTable[i, j]) + "\t";
+                }
+                table += "|\t" + IsTrue(function[i]) + "\t|" + Environment.NewLine;
+            }
+
+            return table;
+        }
+        private string GetPDNF() {
+            string pdnf = "";
+
+            string Implicant(int index) {
+                string implicant = "";
+                for (int i = 0; i < countVariables; i++) {
+                    implicant += namesVariables[i] + (truthTable[index, i] ? "" : overline.ToString());
+                }
+                return implicant;
+            }
+
+            for (int i = 0; i < function.Length; i++) {
+                if (function[i]) {
+                    pdnf += Implicant(i) + (i == function.Length - 1 ? "" : " ∨ ");
+                }
+            }
+
+            return pdnf;
+        }
+
+        public Function GetDualFunction() {
+            int[] func = new int[rows];
+            for (int i = 0; i < rows; i++) {
+                func[i] = function[rows - 1 - i] ? 0 : 1;
+            }
+            
+            return new Function(func);
+        }
+
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
+        public override bool Equals(object obj) {
+            if (countVariables != ((Function)obj).countVariables)
+                return false;
+
+            for (int i = 0; i < countVariables; i++) {
+                if (function[i] != ((Function)obj).function[i])
+                    return false;
+            }
+
+            return true;
+        }
+        public static bool operator ==(Function func1, Function func2) {
+            return func1.Equals(func2);
+        }
+        public static bool operator !=(Function func1, Function func2) {
+            return !func1.Equals(func2);
+        }
+
+        public static bool Implication(bool x, bool y) {
+            return !x || y;
+        }
+        public static bool ArrowPier(bool x, bool y) {
+            return !(x || y);
+        }
+        public static bool ShefferStroke(bool x, bool y) {
+            return !(x && y);
+        }
+
+        public static bool[,] AllFunction(int countVariables) {
+            bool[,] allFunc = new bool[(int)Math.Pow(2, 2 * countVariables), (int)Math.Pow(2, countVariables)];
+
+            for (int i = 0; i < (int)Math.Pow(2, 2 * countVariables); i++) {
+                string binaryNumber = GetBinaryNumber(i, countVariables);
+
+                for (int j = 0; j < (int)Math.Pow(2, countVariables); j++) {
+                    allFunc[i, j] = int.Parse(binaryNumber[j] + "") == 1;
+                }
+            }
+
+            return allFunc;
+        }
+    }
+}
---
+++ Early access/MatrixCalculator.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixCalculator {
+    class Program {
+        static void Main(string[] args) {
+
+            Matrix A = new Matrix(new double[,] { { 1, 13, 5, 7 }, { 2, 4, 6, 28 } });
+            Matrix B = new Matrix(new double[,] { { 0, 13, -2, -4 }, { -1, 2, 3, -5 } });
+
+            Console.WriteLine((A-2*B).Transpose());
+
+            Console.Read();
+
+        }
+    }
+
+    class Matrix {
+
+        public int Lines {
+            get {
+                return matrix.GetLength(0);
+            }
+        }
+        public int Columns {
+            get {
+                return matrix.GetLength(1);
+            }
+        }
+
+        private double[,] matrix;
+
+        private Matrix(int lines, int columns) {
+            matrix = new double[lines, columns];
+        }
+        public Matrix(double[,] data) {
+            matrix = data;
+        }
+        public Matrix() {
+            matrix = new double[0, 0];
+        }
+
+        public double this[int i, int j] {
+            get {
+                return matrix[i, j];
+            }
+        }
+
+        public static Matrix empty {
+            get {
+                return new Matrix();
+            }
+        }
+
+        public bool isSymmetrical {
+            get {
+                return this == Transpose();
+            }
+        }
+        public bool isSkewSymmetric {
+            get {
+                return this == -Transpose();
+            }
+        }
+        public bool isDiagonal {
+            get {
+                bool isD = true;
+
+                for (int i = 0; i < Lines; i++) {
+                    for (int j = 0; j < Columns; j++) {
+                        if (i != j && matrix[i, j] != 0) {
+                            isD = false;
+                            break;
+                        }
+                    }
+                }
+
+                return isSquare && isD;
+            }
+        }
+        public bool isUpper {
+            get {
+                bool isU = true;
+
+                for (int i = 0; i < Lines; i++) {
+                    for (int j = 0; j < Columns; j++) {
+                        if (i > j && matrix[i, j] != 0) {
+                            isU = false;
+                            break;
+                        }
+                    }
+                }
+
+                return isSquare && isU;
+            }
+        }
+        public bool isDown {
+            get {
+                bool isD = true;
+
+                for (int i = 0; i < Lines; i++) {
+                    for (int j = 0; j < Columns; j++) {
+                        if (i < j && matrix[i, j] != 0) {
+                            isD = false;
+                            break;
+                        }
+                    }
+                }
+
+                return isSquare && isD;
+            }
+        }
+        public bool isSquare {
+            get {
+                return Lines == Columns;
+            }
+        }
+
+
+        public static Matrix operator +(Matrix leftOperand, Matrix rightOperand) {
+            if (leftOperand.Lines == rightOperand.Lines && leftOperand.Columns == rightOperand.Columns) {
+                Matrix newMatrix = new Matrix(leftOperand.Lines, leftOperand.Columns);
+
+                for (int i = 0; i < newMatrix.Lines; i++) {
+                    for (int j = 0; j < newMatrix.Columns; j++) {
+                        newMatrix.matrix[i, j] = leftOperand.matrix[i, j] + rightOperand.matrix[i, j];
+                    }
+                }
+
+                return newMatrix;
+            } else return new Matrix();
+        }
+
+        public static Matrix operator -(Matrix leftOperand, Matrix rigthOperand) {
+            return leftOperand + (rigthOperand * -1);
+        }
+        public static Matrix operator -(Matrix operand) {
+            return operand * (-1);
+        }
+        
+        public static Matrix operator *(double scalar, Matrix matrix) {
+            return scalarMultiplication(scalar, matrix);
+        }
+        public static Matrix operator *(Matrix matrix, double scalar) {
+            return scalarMultiplication(scalar, matrix);
+        }
+        private static Matrix scalarMultiplication(double scalar, Matrix matrix) {
+            Matrix newMatrix = new Matrix(matrix.Lines, matrix.Columns);
+            for (int i = 0; i < newMatrix.Lines; i++) {
+                for (int j = 0; j < newMatrix.Columns; j++) {
+                    newMatrix.matrix[i, j] = matrix.matrix[i, j] * scalar;
+                }
+            }
+            return newMatrix;
+        }
+
+        public static Matrix operator /(double scalar, Matrix matrix) {
+            return scalarDivision(scalar, matrix);
+        }
+        public static Matrix operator /(Matrix matrix, double scalar) {
+            return scalarDivision(scalar, matrix);
+        }
+        private static Matrix scalarDivision(double scalar, Matrix matrix) {
+            return scalarMultiplication(1 / scalar, matrix);
+        }
+
+        public Matrix Transpose() {
+            double[,] newMatrix = new double[Columns, Lines];
+
+            int iMax = Lines, jMax = Columns;
+            for (int i = 0; i < iMax; i++) {
+                for (int j = 0; j < jMax; j++) {
+                    newMatrix[j, i] = matrix[i, j];
+                }
+            }
+
+            return new Matrix(newMatrix);
+        }
+
+        public static Matrix operator *(Matrix leftOperand, Matrix rigthOperand) {
+            if (leftOperand.Columns == rigthOperand.Lines) {
+
+                Matrix newMatrix = new Matrix(leftOperand.Lines, rigthOperand.Columns);
+
+                for (int i = 0; i < newMatrix.Lines; i++) {
+                    for (int j = 0; j < newMatrix.Columns; j++) {
+
+                        for (int k = 0; k < leftOperand.Columns; k++) {
+                            newMatrix.matrix[i, j] += leftOperand.matrix[i, k] * rigthOperand.matrix[k, j];
+                        }
+                        
+                    }
+                }
+
+                return newMatrix;
+
+            } else return new Matrix();
+        }
+
+        public static Matrix operator ^(Matrix matrix, int degree) {
+            Matrix newMatrix = GetSingleMatrix(matrix.Lines, matrix.Columns);
+            if (degree > 0) {
+                for (int i = 0; i < degree; i++) {
+                    newMatrix *= matrix;
+                }
+
+                return newMatrix;
+            } else if (degree == 0) {
+                return GetSingleMatrix(matrix.Lines, matrix.Columns);
+            } else return matrix;
+         }
+
+        public static bool operator ==(Matrix leftOperand, Matrix rigthOperand) {
+            if (leftOperand.Lines == rigthOperand.Lines && rigthOperand.Columns == rigthOperand.Columns) {
+                for (int i = 0; i < leftOperand.Lines; i++) {
+                    for (int j = 0; j < leftOperand.Columns; j++) {
+                        if (leftOperand[i, j] != rigthOperand[i, j]) {
+                            return false;
+                        }
+                    }
+                }
+            } else return false;
+
+            return true;
+        }
+        public static bool operator !=(Matrix leftOperand, Matrix rigthOperand) {
+            return !(leftOperand == rigthOperand);
+        }
+
+        public static Matrix GetSingleMatrix(int lines, int columns) {
+            double[,] E = new double[lines, columns];
+
+            for (int i = 0; i < lines; i++) {
+                for (int j = 0; j < columns; j++) {
+                    if (i == j)
+                        E[i, j] = 1;
+                    else E[i, j] = 0;
+                }
+            }
+
+            return new Matrix(E);
+        }
+
+
+        public string GetElemet(int i, int j) {
+            if (i <= Lines && j <= Columns)
+                return $"Элемент [{i}, {j}] = {matrix[i - 1, j - 1]}";
+            else return "Элемент не найден";
+        }
+
+        public override string ToString() {
+            string tmp = "";
+
+            for (int i = 0; i < Lines; i++) {
+                for (int j = 0; j < Columns; j++) {
+                   tmp += this[i, j] + "\t";
+                }
+                tmp += "\n";
+            }
+
+            return tmp;
+        }
+    }
+}
